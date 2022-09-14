cd81877b0d8d11d9ca1663bd8f85abafdd676816
voroscsoki <wcsongor02@gmail.com>
2022-09-15 00:33:01 +0200

    First two tasks

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while(true){
+            String[] cmd = s.nextLine().split(" ");
+
+            if (cmd[0].equals("exit")) {
+                exit(cmd);
+            }
+            else if (cmd[0].equals("hello")) {
+                hello(cmd);
+            }
+        }
+    }
+
+    protected static void exit(String[] cmd){
+        System.exit(0);
+    }
+    protected static void hello(String[] cmd){
+        System.out.println("Hello world!");
     }
 }
