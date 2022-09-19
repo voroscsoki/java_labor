877888180f7c8c3050eec8756e9af4a961974058
voroscsoki <wcsongor02@gmail.com>
2022-09-19 17:06:48 +0200

    Task 5 (improved folder navigation)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Objects;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
     private static File wd = new File(System.getProperty("user.dir"));
@@ -17,6 +15,7 @@ public class Main {
                     case "hello" -> hello(cmd);
                     case "pwd" -> pwd(cmd);
                     case "ls" -> ls(cmd);
+                    case "cd" -> cd(cmd);
                 }
             }
             catch (Exception e){
@@ -29,16 +28,14 @@ public class Main {
         System.exit(0);
     }
     protected static void hello(String[] cmd){ System.out.println("Hello world!"); }
-    protected static void pwd(String[] cmd) throws FileNotFoundException {
-        String current = System.getProperty("user.dir");
-        if(current == null) {
-            throw new FileNotFoundException("Nem létező mappa!");
-        }
+    protected static void pwd(String[] cmd) throws IOException {
+        String current = wd.getCanonicalPath(); //throws
         File currentFolder = new File(current);
         System.out.println("Jelenlegi mappa: " + current);
         System.out.println("Fájlok és almappák száma: " + Objects.requireNonNull(currentFolder.listFiles()).length);
     }
     protected static void ls(String[] cmd) throws FileNotFoundException {
+        boolean detailed = cmd.length > 1 && cmd[1].equals("-l");
         if(wd == null){
             throw new FileNotFoundException("Nem létező mappa!");
         }
@@ -48,7 +45,38 @@ public class Main {
         }
         Arrays.sort(list, Comparator.comparing(File::getName)); //masszívan felesleges
         for (File file: list) {
-            System.out.println(((file.isDirectory()) ? "M\t" : "F\t") + file.getName());
+            if(detailed)
+                System.out.println((file.isDirectory()? "D\t" : "F\t") + file.getName() + " -- " + fileOrDirSize(file));
+            else
+                System.out.println(file.getName());
         }
+    }
+    protected static long fileOrDirSize(File f){
+        long length = 0;
+        if(f.isFile()) return f.length();
+
+        File[] fileList = f.listFiles();
+        if(fileList == null) return 0;
+
+        for (File file : fileList){
+            if(file.isFile()) length += file.length();
+            else length += fileOrDirSize(file);
+        }
+        return length;
+    }
+
+    protected static void cd(String[] cmd) throws IOException {
+        String target = cmd[1];
+        if(target.equals("..")){
+            File next = wd.getParentFile();
+            if(next != null) wd = next;
+            return;
+        }
+        File[] files = wd.listFiles();
+        if(files == null) throw new IOException("Fájllistázás nem sikerült!");
+        Optional<File> targetFolder = Arrays.stream(files)
+            .filter(f -> f.isDirectory() && f.getName().equals(target))
+            .findFirst();
+        if(targetFolder.isPresent()) wd = targetFolder.get();
     }
 }
