394d966c35ed4c7bb1b16d89fc76610637a3522c
voroscsoki <wcsongor02@gmail.com>
2022-10-03 14:38:27 +0200

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Beer gui = new Beer("Guiness", "stout", 4.2);
         Beer kobi = new Beer("Kőbányai", "ale", 4.3);
         System.out.println(gui);
         System.out.println(kobi);
+
+        String line;
+        Scanner r = new Scanner(System.in);
+        while(true){
+            line = r.nextLine();
+            String[] words = line.split(" ");
+            if(words[0].equals("exit")) { return; }
+        }
     }
 }
