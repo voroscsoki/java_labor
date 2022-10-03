c04e88ae0573076ddd61233e9778a5bc258bbda0
voroscsoki <wcsongor02@gmail.com>
2022-10-03 15:04:19 +0200

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,41 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    private static ArrayList<Beer> beers = new ArrayList<>();
     public static void main(String[] args) {
-        Beer gui = new Beer("Guiness", "stout", 4.2);
-        Beer kobi = new Beer("Kőbányai", "ale", 4.3);
-        System.out.println(gui);
-        System.out.println(kobi);
-
         String line;
+        beers.add(new Beer("Guiness", "stout", 4.2));
+        beers.add(new Beer("Kőbányai", "ale", 4.3));
+
         Scanner r = new Scanner(System.in);
         while(true){
             line = r.nextLine();
-            String[] words = line.split(" ");
-            if(words[0].equals("exit")) { return; }
+            String[] cmd = line.split(" ");
+            try{
+                if ("exit".equals(cmd[0])) {
+                    return;
+                }
+                else if ("add".equals(cmd[0])) {
+                    add(cmd);
+                }
+                else if ("list".equals(cmd[0])) {
+                    list(cmd);
+                }
+            }
+            catch(Exception e){
+                System.out.println("Hiba! " + e.getMessage());
+            }
+        }
+    }
+
+    protected static void add(String[] cmd) throws IllegalArgumentException {
+        if(cmd.length < 4) throw new IllegalArgumentException("Túl kevés paraméter!");
+        beers.add(new Beer(cmd[1], cmd[2], Double.parseDouble(cmd[3])));
+    }
+    protected static void list(String[] cmd) throws IllegalArgumentException {
+        for (Beer b : beers) {
+            System.out.println(b);
         }
     }
 }
