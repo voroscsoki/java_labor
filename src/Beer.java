743b45aaa04bce28ca7481a94b7921bb3371bf26
voroscsoki <wcsongor02@gmail.com>
2022-10-03 14:28:12 +0200

    Task 1

---
+++ src/Beer.java
@@ -0,0 +1,30 @@
+public class Beer {
+    private String name;
+    private String style;
+    private double strength;
+
+    @Override
+    public String toString() {
+        return "Name=" + name +
+                ", style=" + style +
+                ", strength=" + strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public double getStrength() {
+        return strength;
+    }
+
+    public Beer(String name, String style, double strength) {
+        this.name = name;
+        this.style = style;
+        this.strength = strength;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
 public class Main {
-
+    public static void main(String[] args) {
+        Beer gui = new Beer("Guiness", "stout", 4.2);
+        Beer kobi = new Beer("Kőbányai", "ale", 4.3);
+        System.out.println(gui);
+        System.out.println(kobi);
+    }
 }
