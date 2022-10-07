35d5de91829f44e74c6f614e1cfc747bd78c7675
voroscsoki <wcsongor02@gmail.com>
2022-10-07 21:18:29 +0200

    Task 2

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,13 @@
 public class Application {
     public static void main(String[] args){
+        Producer p1 = new Producer("elso");
+        Producer p2 = new Producer("masodik");
         try {
-            Producer p = new Producer("teszt");
-            p.go();
+            p1.start();
+            Thread.sleep(500);
+            p2.start();
+        } catch (InterruptedException e) {
+            //unhandled
         }
-        catch (Exception e) {/*unhandled*/ }
     }
 }
--- src/Producer.java
+++ src/Producer.java
@@ -1,15 +1,22 @@
-public class Producer {
+public class Producer extends Thread {
     private final String text;
 
     public Producer(String text) {
         this.text = text;
     }
 
-    void go() throws InterruptedException {
+    public void run() {
         int i = 0;
+
+        //noinspection InfiniteLoopStatement
         while (true) {
             System.out.println(text + " " + (++i) + " " + System.currentTimeMillis() % 100000);
-            Thread.sleep(1000);
+            try {
+                //noinspection BusyWait
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
