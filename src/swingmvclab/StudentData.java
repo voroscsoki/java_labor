ba3ab6e84cd9a17beea6f6c629286b65ef0b5f38
voroscsoki <wcsongor02@gmail.com>
2022-11-06 05:59:08 +0100

    Task 3

--- src/swingmvclab/StudentData.java
+++ src/swingmvclab/StudentData.java
@@ -13,7 +13,7 @@ public class StudentData extends AbstractTableModel {
      * Ez a tagváltozó tárolja a hallgatói adatokat.
      * NE MÓDOSÍTSD!
      */
-    List<Student> students = new ArrayList<Student>();
+    List<Student> students = new ArrayList<>();
 
     @Override
     public int getRowCount() {
@@ -33,6 +33,25 @@ public class StudentData extends AbstractTableModel {
             case 1 -> student.getNeptun();
             case 2 -> student.hasSignature();
             default -> student.getGrade();
+        };
+    }
+
+    @Override
+    public String getColumnName(int columnIndex){
+        return switch (columnIndex){
+            case 0 -> "Név";
+            case 1 -> "Neptun";
+            case 2 -> "Aláírás";
+            default -> "Jegy";
+        };
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex){
+        return switch (columnIndex){
+            case 2 -> Boolean.class;
+            case 3 -> Integer.class;
+            default -> String.class;
         };
     }
 }
