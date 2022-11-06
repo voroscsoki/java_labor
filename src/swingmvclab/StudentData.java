92428596972d14ca296b9f4c544e94069d2b029e
voroscsoki <wcsongor02@gmail.com>
2022-11-06 05:10:07 +0100

    Initial commit

---
+++ src/swingmvclab/StudentData.java
@@ -0,0 +1,17 @@
+package swingmvclab;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * A hallgatók adatait tároló osztály.
+ */
+public class StudentData {
+
+    /*
+     * Ez a tagváltozó tárolja a hallgatói adatokat.
+     * NE MÓDOSÍTSD!
+     */
+    List<Student> students = new ArrayList<Student>();
+    
+}
---
+++ src/swingmvclab/StudentFrame.java
@@ -0,0 +1,86 @@
+package swingmvclab;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import javax.swing.JFrame;
+
+/*
+ * A megjelenítendõ ablakunk osztálya.
+ */
+public class StudentFrame extends JFrame {
+    
+    /*
+     * Ebben az objektumban vannak a hallgatói adatok.
+     * A program indulás után betölti az adatokat fájlból, bezáráskor pedig kimenti oda.
+     * 
+     * NE MÓDOSÍTSD!
+     */
+    private StudentData data;
+    
+    /*
+     * Itt hozzuk létre és adjuk hozzá az ablakunkhoz a különbözõ komponenseket
+     * (táblázat, beviteli mezõ, gomb).
+     */
+    private void initComponents() {
+        this.setLayout(new BorderLayout());
+        
+        // ...
+    }
+
+    /*
+     * Az ablak konstruktora.
+     * 
+     * NE MÓDOSÍTSD!
+     */
+    @SuppressWarnings("unchecked")
+    public StudentFrame() {
+        super("Hallgatói nyilvántartás");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        // Induláskor betöltjük az adatokat
+        try {
+            data = new StudentData();
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("students.dat"));
+            data.students = (List<Student>)ois.readObject();
+            ois.close();
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        // Bezáráskor mentjük az adatokat
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                try {
+                    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("students.dat"));
+                    oos.writeObject(data.students);
+                    oos.close();
+                } catch(Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        // Felépítjük az ablakot
+        setMinimumSize(new Dimension(500, 200));
+        initComponents();
+    }
+
+    /*
+     * A program belépési pontja.
+     * 
+     * NE MÓDOSÍTSD!
+     */
+    public static void main(String[] args) {
+        // Megjelenítjük az ablakot
+        StudentFrame sf = new StudentFrame();
+        sf.setVisible(true);
+    }
+}
